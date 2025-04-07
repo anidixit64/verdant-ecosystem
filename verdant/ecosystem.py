811cff55fbb555ac549c763f5d161c1cacd93260
Aniket Dixit <anidixit@umich.edu>
2025-04-07 09:31:05 -0400

    Combined ecosystem runner function (closes #9)

--- verdant/ecosystem.py
+++ verdant/ecosystem.py
@@ -1,8 +1,21 @@
+from verdant.species import Species
+
+
 class Ecosystem:
     def __init__(self, name, weather="temperate"):
         self.name = name
         self.weather = weather
         self.species = []
 
-    def add_species(self, species_name):
-        self.species.append(species_name)
+    def add_species(self, species):
+        if isinstance(species, Species):
+            self.species.append(species)
+
+    def simulate(self):
+        for s in self.species:
+            s.simulate_growth()
+
+    def __str__(self):
+        return (
+            f"Ecosystem: {self.name} ({self.weather}) with {len(self.species)} species"
+        )
